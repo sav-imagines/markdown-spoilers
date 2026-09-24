0a336fa408588b06a90f0b67b3c08e7fdf167785
Savannah <124691102+sav-imagines@users.noreply.github.com>
2026-09-24 22:03:04 +0200

    Cleanup code

--- lua/markdown-spoilers.lua
+++ lua/markdown-spoilers.lua
@@ -31,6 +31,52 @@ function M.say_hello()
 	vim.print("Hello")
 end
 
+function M.update_spoilers()
+	local cursor_pos = vim.api.nvim_win_get_cursor(0)
+	local current_buf_idx = vim.api.nvim_get_current_buf()
+
+	-- remove previous highlights
+	vim.api.nvim_buf_clear_namespace(current_buf_idx, EXTMARK_NS, 0, -1)
+
+	local lines = vim.api.nvim_buf_get_lines(current_buf_idx, 0, -1, false)
+
+	local comments = {}
+	for i, line in pairs(lines) do
+		if line == nil then
+			break
+		end
+		-- positions with '||' in line
+		local comments_in_line = line:find_match_indeces("||")
+		local comment_count = #comments_in_line
+
+		-- round down to nearest even number (ignore unclosed final ones, no multi-line spoilers)
+		if comment_count % 2 ~= 0 then
+			comment_count = comment_count - 1
+		end
+
+		if comment_count > 0 then
+			for j = 1, comment_count, 2 do
+				local start_col = comments_in_line[j] - 1
+				local end_col = comments_in_line[j + 1] + 1
+				local is_hovered = i == cursor_pos[1] and (start_col <= cursor_pos[2] and cursor_pos[2] < end_col)
+
+				if not is_hovered then
+					vim.api.nvim_buf_set_extmark(current_buf_idx, EXTMARK_NS, i - 1, comments_in_line[j] - 1, {
+						end_line = i - 1,
+						end_col = comments_in_line[j + 1] + 1,
+						hl_group = HL_NAME,
+					})
+					table.append(comments, {
+						line_idx = i,
+						start_idx = comments_in_line[j],
+						end_idx = comments_in_line[(j + 1)],
+					})
+				end
+			end
+		end
+	end
+end
+
 function table.append(t, value)
 	t[#t + 1] = value
 end
@@ -61,61 +107,10 @@ function M.setup(opts)
 	-- Merge user options with defaults
 	opts = opts or {}
 
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		pattern = "*",
-		callback = function()
-			print("Setting Highlight group")
-		end,
-	})
-	-- Create the user command
-	vim.api.nvim_create_user_command("HelloWorld", M.say_hello, {})
-
+	-- on any of these, update the highlights
 	vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "CursorMoved" }, {
-		pattern = { "*.md", "*", "*.md" },
-		callback = function(_)
-			local cursor_pos = vim.api.nvim_win_get_cursor(0)
-			local current_buf_idx = vim.api.nvim_get_current_buf()
-			vim.api.nvim_buf_clear_namespace(current_buf_idx, EXTMARK_NS, 0, -1)
-			local lines = vim.api.nvim_buf_get_lines(current_buf_idx, 0, -1, false)
-			local comments = {}
-			for i, line in pairs(lines) do
-				if line == nil then
-					break
-				end
-				local comments_in_line = line:find_match_indeces("||")
-				local comment_count = #comments_in_line
-
-				-- round down to nearest even number
-				if comment_count % 2 ~= 0 then
-					comment_count = comment_count - 1
-				end
-
-				if comment_count > 0 then
-					for j = 1, comment_count, 2 do
-						local start_col = comments_in_line[j] - 1
-						local end_col = comments_in_line[j + 1] + 1
-						local is_hovered = i == cursor_pos[1]
-							and (start_col <= cursor_pos[2] and cursor_pos[2] <= end_col)
-
-						if not is_hovered then
-							vim.api.nvim_buf_set_extmark(current_buf_idx, EXTMARK_NS, i - 1, comments_in_line[j] - 1, {
-								end_line = i - 1,
-								end_col = comments_in_line[j + 1] + 1,
-								hl_group = HL_NAME,
-							})
-							table.append(comments, {
-								line_idx = i,
-								start_idx = comments_in_line[j],
-								end_idx = comments_in_line[(j + 1)],
-							})
-						end
-					end
-				end
-			end
-			-- for _, comment in pairs(comments) do
-			-- print(string.format("%i: %i-%i", comment.line_idx, comment.start_idx, comment.end_idx))
-			-- end
-		end,
+		pattern = { "*.md" },
+		callback = M.update_spoilers,
 	})
 
 	-- Set up a key mapping
